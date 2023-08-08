acc9574f9e4a3b3c3f856dc826d8a410b8009ed2
DEMIYISADE <bidemi.olayisade@gmail.com>
2023-08-08 04:37:20 +0100

    Updating responsiveness of the app
    Responsiveness using media queries

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Weather from "./Weather";
 
 function App() {
   return (
+    <div className = "wallpaper">
     <div className="App">
       <div className = "container">
         <Weather defaultCity = "Paris"/>
       <footer className="App-footer">
       </footer>
       </div>
+    </div>
     </div>
   );
 }
